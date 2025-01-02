ae110f9de0e94ceeefdf10b792b31b4f53da91ab
Louis Otu <louis.otu@kuda.com>
2025-01-02 04:17:25 -0500

    project setup for hotelier

---
+++ hotelier-core-app.API/Controllers/UserController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace hotelier_core_app.Presentation.Controllers
+{
+    [Route("api/v1/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+    }
+}
