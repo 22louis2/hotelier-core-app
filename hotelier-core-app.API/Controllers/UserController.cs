2c056c3229f2ab6b9ad3d42a74364234f612e04e
Louis Otu <louis.otu@kuda.com>
2025-01-07 21:09:30 -0600

    change to the controller namespace

--- hotelier-core-app.API/Controllers/UserController.cs
+++ hotelier-core-app.API/Controllers/UserController.cs
@@ -2,7 +2,7 @@
 
 // For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
-namespace hotelier_core_app.Presentation.Controllers
+namespace hotelier_core_app.API.Controllers
 {
     [Route("api/v1/[controller]")]
     [ApiController]
